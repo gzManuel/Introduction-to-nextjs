e6683c7a430fb6b1de6796d7c8e3895e696294a1
Manuel Alejandro Garisto Zuna <garistozm@outlook.com>
2022-11-14 20:32:54 -0300

    refactor: Set the project for deployment

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -4,14 +4,13 @@ import { jsx } from 'theme-ui'
 import Link from 'next/link'
 
 export default ({notes}) => {
-
   return (
     <div sx={{variant: 'containers.page'}}>
       <h1>My Notes</h1>
 
       <div sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
         {notes.map(note => (
-          <div sx={{width: '33%', p: 2}}>
+          <div key={note.id} sx={{width: '33%', p: 2}}>
             <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
               <a sx={{textDecoration: 'none', cursor: 'pointer'}}>
                 <div sx={{variant: 'containers.card',}}>
@@ -30,7 +29,6 @@ export default ({notes}) => {
 export async function getServerSideProps() {
   const res = await fetch(`http://localhost:3000/api/note/`);
   const {data} = await res.json();
-  console.log(data);
   return {
     props: {notes: data}
   }
